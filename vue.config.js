da2b82bfcb415ef01fff671be9d3ccf887ea0374
maziyar <maziyaraccwork@gmail.com>
2022-07-13 02:31:57 +0430

    Project initial commit. add alias to project and modify folder structure and delete some files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require("path");
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@views": path.resolve(__dirname, "./src/views"),
+        "@img": path.resolve(__dirname, "./src/assets/images"),
+        "@styles": path.resolve(__dirname, "./src/assets/css"),
+      },
+    },
+  },
+};
